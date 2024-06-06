a7d0e211da466924ab018669726c8e4e8cca3338
tivelprotocol <real.ultimex@gmail.com>
2024-06-06 10:26:03 +0700

    price feed integrations v2

---
+++ contracts/integrations/UniswapV2PriceFeedIntegrationV2.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.4;
+
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+import "../interfaces/IPriceFeedIntegration.sol";
+import "../interfaces/IERC20.sol";
+
+contract UniswapV2PriceFeedIntegrationV2 is IPriceFeedIntegration {
+    address public manager;
+    uint256 public constant override PRECISION = 1e30;
+    address public immutable factory;
+    mapping(address => mapping(address => address)) public bridgeToken;
+
+    error Forbidden(address sender);
+    error ZeroAddress();
+
+    event SetManager(address manager);
+    event SetBridgeToken(
+        address indexed tokenIn,
+        address indexed tokenOut,
+        address bridgeToken
+    );
+
+    constructor(address _factory) {
+        factory = _factory;
+        manager = msg.sender;
+    }
+
+    modifier onlyManager() {
+        if (msg.sender != manager) revert Forbidden(msg.sender);
+        _;
+    }
+
+    function setManager(address _newManager) external onlyManager {
+        if (msg.sender == address(0)) revert ZeroAddress();
+        manager = _newManager;
+
+        emit SetManager(_newManager);
+    }
+
+    function setBridgeToken(
+        address _tokenIn,
+        address _tokenOut,
+        address _bridgeToken
+    ) external onlyManager {
+        bridgeToken[_tokenIn][_tokenOut] = _bridgeToken;
+
+        emit SetBridgeToken(_tokenIn, _tokenOut, _bridgeToken);
+    }
+
+    function _quote(
+        uint256 _amount,
+        address _baseToken,
+        address _quoteToken
+    ) internal view returns (uint256 price) {
+        address pair = IUniswapV2Factory(factory).getPair(
+            _baseToken,
+            _quoteToken
+        );
+        if (pair != address(0)) {
+            address token0 = IUniswapV2Pair(pair).token0();
+            (uint256 reserve0, uint256 reserve1, ) = IUniswapV2Pair(pair)
+                .getReserves();
+            (uint256 baseReserve, uint256 quoteReserve) = _baseToken == token0
+                ? (reserve0, reserve1)
+                : (reserve1, reserve0);
+            if (baseReserve > 0)
+                price = (_amount * PRECISION * quoteReserve) / baseReserve;
+        }
+    }
+
+    function getPrice(
+        address _baseToken,
+        address _quoteToken
+    ) external view override returns (uint256 price) {
+        if (_baseToken == _quoteToken) {
+            return PRECISION;
+        }
+
+        address bridge = bridgeToken[_baseToken][_quoteToken];
+        if (bridge == address(0)) {
+            price = _quote(1, _baseToken, _quoteToken);
+        } else {
+            price = _quote(1, _baseToken, bridge);
+            price = _quote(price / PRECISION, bridge, _quoteToken);
+        }
+
+        uint256 baseDecimals = IERC20(_baseToken).decimals();
+        uint256 quoteDecimals = IERC20(_quoteToken).decimals();
+        uint256 basePrec = 10 ** baseDecimals;
+        uint256 quotePrec = 10 ** quoteDecimals;
+        price = (price * basePrec) / quotePrec;
+    }
+}
